531c9e469dc6105b063a5f14ebc1d6933a21eb08
Dominika Gnich <d.gnich@wp.pl>
2017-06-11 13:17:12 +0200

    commit

--- js/eventy.js
+++ js/eventy.js
@@ -1,33 +1,54 @@
-'use strict';
+//'use strict';
+//
+////eventy to wszytsko to co robi użytkownik czyli klnięcie, scrollowanie, hover itp.
+//
+////preventDefault zablokowanie domyślnego działania
+//
+////addEventListner- funkcja nasłuchaująca czy dane zdarzenie sie wykonało
+//
+//var secondLink = document.getElementsByTagName('a')[1]; //tworzymy zmienną z drugim linkiem
+//
+//console.log(secondLink);
+//
+//function alarm(e){
+    'use strict';
+//   e.preventDefault();//blokujemy domyślną funkcje 
+//    console.log('kliknięto kolejno link');
+//    
+//    console.log(e.type);
+//}
+//
+//secondLink.onclick = alarm; //wywołujemy funkcje alarm() na drugim linku po kliknięciu
+//
+//var thirdLink = document.getElementsByTagName('a')[2]; // tworzymy zmienna z 3 linkiem 
+//
+//thirdLink.addEventListener('click', alarm);//wywolujemy funkcje alarm() na trzecim linku po kliknieciu za pomoca event Listenera
+//
+//thirdLink.removeEventListener('click', alarm);//usuwamy Listenera, mozna go usunąc bo jest nazwa funkcji, fuknkcji anonimowych nie da sie usunac. 
 
-//eventy to wszytsko to co robi użytkownik czyli klnięcie, scrollowanie, hover itp.
+//e-obiekt zdarzenia. 
 
-//preventDefault zablokowanie domyślnego działania
 
-//addEventListner- funkcja nasłuchaująca czy dane zdarzenie sie wykonało
+//efekt powiekszania napisu po najechaniu
 
-var secondLink = document.getElementsByTagName('a')[1]; //tworzymy zmienną z drugim linkiem
+var mainHeader = document.getElementById('main-header');//przypisujemy nagłówek strony do zmiennej
 
-console.log(secondLink);
-
-function alarm(e){
-   e.preventDefault();//blokujemy domyślną funkcje 
-    console.log('kliknięto kolejno link');
+function resize(e){ // przekazujemy zdarzenie jako parametr funkcji
     
-    console.log(e.type);
+//    console.log(e.type);
+    if(e.type == 'mouseover'){//sprawdzenie typu zdarzenia 
+     mainHeader.style.fontSize = "70px"; //zmien wielkosć fonta po najechaniu
+    }
+    else{
+        mainHeader.style.fontSize = "inherit"; //przywróc wartość domyślną po zjechaniu 
+    }
 }
 
-secondLink.onclick = alarm; //wywołujemy funkcje alarm() na drugim linku po kliknięciu
-
-var thirdLink = document.getElementsByTagName('a')[2]; // tworzymy zmienna z 3 linkiem 
-
-thirdLink.addEventListener('click', alarm);//wywolujemy funkcje alarm() na trzecim linku po kliknieciu za pomoca event Listenera
-
-thirdLink.removeEventListener('click', alarm);//usuwamy Listenera, mozna go usunąc bo jest nazwa funkcji, fuknkcji anonimowych nie da sie usunac. 
 
 
+mainHeader.onmouseover = resize; //wywaolanie funkcji resize po najechaniu myszka
 
-
+mainHeader.onmouseout = resize; //wywolanie funkcji resize po zjechaniu myszką z nagowka 
 
 
 
